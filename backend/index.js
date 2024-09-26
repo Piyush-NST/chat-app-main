b6cb56921c722424c0f78333247ef100ef238739
Piyush-NST <piyushkaushal238@gmail.com>
2024-09-27 00:25:38 +0530

    upated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app-nq0.pages.dev/',
     credentials:true
 };
 app.use(cors(corsOption)); 
